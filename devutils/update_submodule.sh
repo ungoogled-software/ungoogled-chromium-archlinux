e3b1b1892fb1825d18c11dfd3db0741e8d04a83c
Albert Tang <tangalbert919@yahoo.com>
2019-08-27 15:40:30 -0500

    Update devutils/update_submodule.sh

--- devutils/update_submodule.sh
+++ devutils/update_submodule.sh
@@ -2,8 +2,18 @@
 
 UNGOOGLED_REPO=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))/ungoogled-chromium
 
-CHROMIUM_VERSION=$(curl -sL https://raw.githubusercontent.com/Eloston/ungoogled-chromium/master/chromium_version.txt)
-UNGOOGLED_REVISION=$(curl -sL https://raw.githubusercontent.com/Eloston/ungoogled-chromium/master/revision.txt)
+# Ensure the submodule is initialized.
+git submodule init
+git submodule update
+
+# Pull latest commits and get the tag submodule is on.
+cd $UNGOOGLED_REPO
+CURRENT_TAG=$(git describe)
+git pull -q
+cd ..
+
+CHROMIUM_VERSION=$(git --git-dir=ungoogled-chromium/.git show origin/master:chromium_version.txt)
+UNGOOGLED_REVISION=$(git --git-dir=ungoogled-chromium/.git show origin/master:revision.txt)
 
 if [ UNGOOGLED_REVISION != "0" ]
 then
@@ -11,18 +21,15 @@ then
 else
     UPDATED_TAG="${CHROMIUM_VERSION}"
 fi
-    
-# Ensure the submodule is initialized.
-git submodule init
-git submodule update
 
 # Update the submodule.
 cd $UNGOOGLED_REPO
-CURRENT_TAG=$(git describe)
 if [ CURRENT_TAG == UPDATED_TAG ]
 then
     echo "Submodule already on latest version."
 else
     git checkout $UPDATED_TAG
+    cd ..
+    git add ungoogled-chromium
     echo "Submodule updated. Commit the change when you can."
 fi
