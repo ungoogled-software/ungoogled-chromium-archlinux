1bff97a887059bfc61398f4b8014f212852600b7
networkException <git@nwex.de>
2021-05-14 19:45:49 +0200

    CI: Replace building stage with TypeScript Action
    This extracts logic to control the build container
    in a stage to a local Action written in TypeScript.
    
    Compared to the previous YAML steps this is a lot
    more scalable, easier to maintain and allows for
    more complicated logic.
    
    Additionally, as pull requests do not have permission
    to push to the container registry, the container image
    now gets distributed to each stage using an artifact if
    required.

---
+++ .github/actions/stage/index.ts
@@ -0,0 +1,89 @@
+import * as artifactHost from '@actions/artifact';
+import { ArtifactClient, DownloadResponse, UploadResponse } from '@actions/artifact';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import { ExecOptions } from '@actions/exec';
+import * as github from '@actions/github';
+import { readdirSync } from 'fs';
+
+const shell = async (commandLine: string, args?: Array<string>, options?: ExecOptions): Promise<void> => {
+    const code: number = await exec.exec(commandLine, args, options);
+
+    if (code !== 0)
+        throw new Error(`Stage: A ${commandLine} command errored with code ${code}`);
+};
+
+(async () => {
+    const output = () => {
+        console.log('Stage: Setting output', {
+            'finished': input.finished,
+            'chromium-version': input.chromiumVersion,
+            'use-registry': input.useRegistry,
+            'image-tag': input.imageTag
+        });
+
+        core.setOutput('finished', input.finished);
+        core.setOutput('chromium-version', input.chromiumVersion);
+        core.setOutput('use-registry', input.useRegistry);
+        core.setOutput('image-tag', input.imageTag);
+    };
+
+    const artifact: ArtifactClient = artifactHost.create();
+    const input = {
+        finished: core.getInput('finished') === 'true',
+        progressName: core.getInput('progress-name'),
+        chromiumVersion: core.getInput('chromium-version', { required: true }),
+        useRegistry: core.getInput('use-registry') === 'true',
+        registryToken: core.getInput('registry-token'),
+        imageTag: core.getInput('image-tag')
+    };
+
+    console.log('Stage: Got input', input);
+
+    if (input.finished)
+        return output();
+
+    if (input.useRegistry) {
+        await core.group<void>('Stage: Logging into docker registry...', () =>
+            shell('docker', ['login', 'docker.pkg.github.com', '-u', github.context.actor, '-p', input.registryToken]));
+
+        await core.group<void>('Stage: Pulling image from registry...', () =>
+            shell('docker', ['pull', input.imageTag]));
+    } else {
+        await core.group<DownloadResponse>('Stage: Downloading image artifact...', () =>
+            artifact.downloadArtifact('image'));
+
+        await core.group<void>('Stage: Loading image from file...', () =>
+            shell('docker load --input image'));
+    }
+
+    await core.group<void>('Stage: Creating input, output and progress directory...', () =>
+        shell('mkdir input output progress'));
+
+    if (input.progressName !== '') {
+        await core.group<DownloadResponse>('Stage: Downloading progress artifact...', () =>
+            artifact.downloadArtifact(input.progressName));
+
+        await core.group<void>('Stage: Moving progress archive into input directory...', () =>
+            shell('mv progress.tar.zst progress.tar.zst.sum input'));
+    }
+
+    const mount = (directory: string): Array<string> => ['--mount', `type=bind,source=${process.cwd()}/${directory},target=/mnt/${directory}`];
+
+    await core.group<void>('Stage: Running docker container...', () =>
+        shell('docker', ['run', '-e', 'TIMEOUT=330', ...mount('input'), ...mount('output'), ...mount('progress'), input.imageTag]));
+
+    if (readdirSync('output').length !== 0) {
+        console.log('Stage: Successfully built package');
+        input.finished = true;
+    }
+
+    await core.group<UploadResponse>('Stage: Uploading progress...', () =>
+        artifact.uploadArtifact(github.context.job, readdirSync('progress').map(node => `progress/${node}`), 'progress'));
+
+    if (input.finished)
+        await core.group<UploadResponse>('Stage: Uploading package...', () =>
+            artifact.uploadArtifact(input.chromiumVersion, readdirSync('output').map(node => `output/${node}`), 'output'));
+
+    output();
+})().catch(core.setFailed);
